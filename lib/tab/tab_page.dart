4d3bae8a288597ba398139fcb991ecf648b0a96e
minhaneul <onlynforever@gmail.com>
2023-02-26 13:42:02 +0900

    add: tab page에 화면 연결

--- lib/tab/tab_page.dart
+++ lib/tab/tab_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'account/account_page.dart';
+import 'home/home_page.dart';
+import 'search/search_page.dart';
+
 class TabPage extends StatefulWidget {
   const TabPage({Key? key}) : super(key: key);
 
@@ -10,30 +14,39 @@ class TabPage extends StatefulWidget {
 class _TabPageState extends State<TabPage> {
   int _currentIndex = 0;
 
+  final _pages = const [
+    HomePage(),
+    SearchPage(),
+    AccountPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Account',
-          ),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: _pages[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Account',
+            ),
+          ],
+        ),
       ),
     );
   }
