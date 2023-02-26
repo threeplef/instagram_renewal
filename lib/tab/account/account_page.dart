6cb392308344b41a36ebdf9273989213f5442151
minhaneul <onlynforever@gmail.com>
2023-02-26 14:01:06 +0900

    add: account page UI 작성

--- lib/tab/account/account_page.dart
+++ lib/tab/account/account_page.dart
@@ -5,6 +5,121 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('account page');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Instagram Clone'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 16.0, 25.0, 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
+                  children: [
+                    const SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_400x400.jpg'),
+                      ),
+                    ),
+                    Container(
+                      width: 90,
+                      height: 90,
+                      alignment: Alignment.bottomRight,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: const [
+                          SizedBox(
+                            width: 25,
+                            height: 25,
+                            child: FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.add),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  '아이유',
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Column(
+                children: const [
+                  Text(
+                    '3',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '게시물',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Column(
+                children: const [
+                  Text(
+                    '0',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '팔로워',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Column(
+                children: const [
+                  Text(
+                    '0',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '팔로잉',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
